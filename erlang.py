4be56030dc0ad3ed2b453b50acd7015200b1fb46
Jairo Sánchez <jairojsh@gmail.com>
2015-11-18 14:15:02 -0600

    Agregados archivos de proyecto para SublimeText 3

--- erlang.py
+++ erlang.py
@@ -2,28 +2,74 @@
 # -*- coding: utf-8 -*-
 # @Author: pixki
 # @Date:   2015-11-11 12:07:40
-# @Last Modified by:   pixki
-# @Last Modified time: 2015-11-11 12:25:02
+# @Last Modified by:   Jairo Sánchez
+# @Last Modified time: 2015-11-18 14:12:15
 
 import numpy as np
-from scipy.stats import expon,erlang
+from scipy.stats import expon, erlang
 import matplotlib.pyplot as plt
-import sys, argparse
+import argparse
+import sys
 
+
+class hyperexp(rv_continuous):
+
+    """ An HyperExponential Random Variable		
+    """
+
+    def _rvs(self):
+        return
+
+    def _pdf(self):
+        return
 
 
 def main():
-	parser=argparse.ArgumentParser()
-	parser.add_argument('-s', '--stages', type=int, required=True, help='Etapas de la distribución')
-	parser.add_argument('-l', '--lambdap', type=float, required=True, help='Parámetro lambda de cada distribución')
-	parser.add_argument('-r', '--runs', type=int, required=True, help='Ejecuciones a realizar por cada simulación')
-	args=parser.parse_args()
-	print 'Simulando distribución Erlang con {0} etapas (lambda={1}) en {2} ejecuciones'.format(args.stages, args.lambdap, args.runs)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-s', '--stages', type=int, required=False,
+                        help='Etapas de la distribución')
+    parser.add_argument('-l', '--lambdap', type=float, required=True,
+                        help='Parámetro lambda de cada distribución')
+    parser.add_argument('-r', '--runs', type=int, required=True,
+                        help='Ejecuciones a realizar por cada simulación')
+    parser.add_argument('-o', '--output', type=str, required=True,
+                        help='Archivo de salida para la grafica')
+    parser.add_argument('-d', '--dist', type=str, required=True,
+                        choices=['erlang', 'expon', 'hyperexp'],
+                        help='Distribución a emplear para la simulación')
+    args = parser.parse_args()
+    msg = 'Distribución {3} con {0} etapas (lambda={1}) en {2} ejecuciones'
+    print msg.format(args.stages, args.lambdap, args.runs, args.dist)
+    fig, ax = plt.subplots(1, 1)
+    if args.dist in 'erlang':
+        if args.stages <= 0:
+            print 'Error: se necesita un número válido de etapas'
+            sys.exit(1)
+        mean, var, skew, kurt = erlang.stats(args.stages, scale=args.lambdap,
+                                             moments='mvsk')
+        print "E[X]={0}, var(X)={1}".format(mean, var)
+        x = np.linspace(erlang.ppf(0.00001, args.stages, scale=args.lambdap),
+                        erlang.ppf(0.99999, args.stages, scale=args.lambdap),
+                        num=1000)
+        rv = erlang(args.stages, scale=args.lambdap)
+        ax.plot(x, rv.pdf(x), 'r-', lw=5, alpha=0.6, label='Erlang PDF')
+        # Generate random numbers with this distribution
+        r = erlang.rvs(args.stages, scale=args.lambdap, size=args.runs)
+        ax.hist(r, bins=20, normed=True, histtype='stepfilled', alpha=0.2)
+    elif args.dist in 'expon':
+        mean, var, skew, kurt = expon.stats(scale=args.lambdap, moments='mvsk')
+        print "E[X]={0}, var(X)={1}".format(mean, var)
+        x = np.linspace(expon.ppf(0.00001, scale=args.lambdap),
+                        expon.ppf(0.99999, scale=args.lambdap),
+                        num=1000)
+        rv = expon(scale=args.lambdap)
+        ax.plot(x, rv.pdf(x), 'r-', lw=5, alpha=0.6, label='Exponential PDF')
+        # Generate random numbers with this distribution
+        r = expon.rvs(scale=args.lambdap, size=args.runs)
+        ax.hist(r, bins=20, normed=True, histtype='stepfilled', alpha=0.2)
+    elif args.dist in 'hyperexp':
+        print "HyperExponential RV"
 
-
-	for run in range(args.runs):
-		print "Run no. {0}...".format(run)
-
-
+    plt.show()
 if __name__ == '__main__':
-	main()
+    main()
--- gamma_example.py
+++ gamma_example.py
@@ -2,40 +2,42 @@
 # -*- coding: utf-8 -*-
 # @Author: pixki
 # @Date:   2015-11-11 12:40:52
-# @Last Modified by:   pixki
-# @Last Modified time: 2015-11-11 14:04:25
+# @Last Modified by:   Jairo Sánchez
+# @Last Modified time: 2015-11-18 11:34:50
 
 import numpy as np
 from scipy.stats import gamma
 import matplotlib.pyplot as plt
 fig, ax = plt.subplots(1, 1)
 
-#Calculate a few first moments:
+# Calculate a few first moments:
 
 a = 5
-lambdap=2
+lambdap = 2
 mean, var, skew, kurt = gamma.stats(a, scale=lambdap, moments='mvsk')
 print "E[X]={0}, var(X)={1}".format(mean, var)
-#Display the probability density function (pdf):
+# Display the probability density function (pdf):
 
-x = np.linspace(gamma.ppf(0.00001, a, scale=lambdap), gamma.ppf(0.99999, a, scale=lambdap), num=1000)
-ax.plot(x, gamma.pdf(x, a, scale=lambdap), 'r-', lw=5, alpha=0.6, label='gamma pdf')
+x = np.linspace(gamma.ppf(0.00001, a, scale=lambdap), gamma.ppf(
+    0.99999, a, scale=lambdap), num=1000)
+ax.plot(x, gamma.pdf(x, a, scale=lambdap), 'r-',
+        lw=5, alpha=0.6, label='gamma pdf')
 
-#Alternatively, the distribution object can be called (as a function) to fix the shape, location and scale parameters. This returns a “frozen” RV object holding the given parameters fixed.
-#Freeze the distribution and display the frozen pdf:
-
+# Alternatively, the distribution object can be called (as a function) to fix
+# the shape, location and scale parameters. This returns a “frozen” RV object
+# holding the given parameters fixed.
+# Freeze the distribution and display the frozen pdf:
 rv = gamma(a, scale=lambdap)
 ax.plot(x, rv.pdf(x), 'k-', lw=2, label='frozen pdf')
 
-#Check accuracy of cdf and ppf:
-#vals = gamma.ppf([0.001, 0.5, 0.999], a, scale=lambdap)
-#np.allclose([0.001, 0.5, 0.999], gamma.cdf(vals, a, scale=lambdap))
+# Check accuracy of cdf and ppf:
+# vals = gamma.ppf([0.001, 0.5, 0.999], a, scale=lambdap)
+# np.allclose([0.001, 0.5, 0.999], gamma.cdf(vals, a, scale=lambdap))
 
-#Generate random numbers:
+# Generate random numbers:
 r = gamma.rvs(a, size=100000, scale=lambdap)
 
-#And compare the histogram:
+# And compare the histogram:
 ax.hist(r, bins=50, normed=True, histtype='stepfilled', alpha=0.2)
 ax.legend(loc='best', frameon=False)
 plt.show()
-
