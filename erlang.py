dfc6cd6c51ac1e5754e8e7bd04c28f7f169621e8
Jairo Sánchez <jairosh@avior.com.mx>
2015-11-25 07:05:21 -0600

    Agregada la clase para la distribución HiperExponencial

--- erlang.py
+++ erlang.py
@@ -2,26 +2,55 @@
 # -*- coding: utf-8 -*-
 # @Author: pixki
 # @Date:   2015-11-11 12:07:40
-# @Last Modified by:   Jairo Sánchez
-# @Last Modified time: 2015-11-18 14:12:15
+# @Last Modified by:   jairo
+# @Last Modified time: 2015-11-24 23:05:40
 
 import numpy as np
-from scipy.stats import expon, erlang
+from scipy.stats import expon, erlang, rv_continuous
 import matplotlib.pyplot as plt
 import argparse
 import sys
 
+import numpy.random as mtrand
+
 
 class hyperexp(rv_continuous):
 
-    """ An HyperExponential Random Variable		
+    """An HyperExponential Random Variable		
     """
 
-    def _rvs(self):
+    def __init__(self, alpha=0.5, lambda1=1.0, lambda2=1.0):
+        self.alpha = alpha
+        self.lambda1 = lambda1
+        self.lambda2 = lambda2
+
+    def rvs(self, size=1):
+        st="Generando {0} muestras con dist. HypExp([{1},{2}],[{3},{4}])"
+        print st.format(size, self.alpha, 1-self.alpha, 
+                        self.lambda1, self.lambda2)
+        vsample = np.vectorize(self._single_sample)
+        return np.fromfunction(vsample, (size,))
+
+    def _single_sample(self, size):
+        U1=mtrand.random()
+        if U1 >= self.alpha:
+            scale = self.lambda1
+        else:
+            scale = self.lambda2
+        U2 = mtrand.random()
+        return -np.log(U2) / scale
+
+    def pdf(self, x):
+        a = self.alpha*self.lambda1*np.exp(self.lambda1*-x)
+        b = (1-self.alpha)*self.lambda2*np.exp(self.lambda2*-x)
+        return a + b
+
+    def ppf(self):
         return
 
-    def _pdf(self):
-        return
+    
+
+
 
 
 def main():
@@ -69,6 +98,12 @@ def main():
         ax.hist(r, bins=20, normed=True, histtype='stepfilled', alpha=0.2)
     elif args.dist in 'hyperexp':
         print "HyperExponential RV"
+        rv=hyperexp(0.5, 1.2, 1.3)
+        values=rv.rvs(size=10)
+        print values
+        print "------------------------"
+        x = np.linspace(0.00000001, 10.99999, num=1000)
+        ax.plot(x, rv.pdf(x), 'r-', lw=5, alpha=0.6, label='HyperExponential PDF')
 
     plt.show()
 if __name__ == '__main__':
